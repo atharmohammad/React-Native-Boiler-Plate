980cef3f47711f13b8dea645b70fce379641385b
atharmohammad <mohd.rule123@gmail.com>
2021-06-21 23:48:39 +0530

    navigation signup login

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,19 +1,42 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Input, Button, Text } from "react-native-elements";
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Input, Text, Button } from "react-native-elements";
 
 export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.wrap}>
       <Text h3 style={{ textAlign: "center" }}>
         Login
       </Text>
-      <Input label="Email" />
-      <Input label="Password" />
-      <Button
-        title="Go to Signup"
-        onPress={() => navigation.navigate("Signup")}
+      <Input
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
       />
+
+      <Input
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Password"
+        secureTextEntry
+      />
+
+      <Button title="Login" />
+      <TouchableOpacity
+        style={styles.link}
+        onPress={() => navigation.navigate("Signup")}
+      >
+        <Text style={{ color: "blue" }}>
+          Dont have a account ? Go to Sign up
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -24,5 +47,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: "80%",
     margin: 10,
+  },
+  link: {
+    marginTop: 10,
   },
 });
--- src/Screens/Signup.js
+++ src/Screens/Signup.js
@@ -1,19 +1,42 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Input, Button, Text } from "react-native-elements";
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Input, Text, Button } from "react-native-elements";
 
-export default function SignUp({ navigation }) {
+export default function Signup({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.wrap}>
       <Text h3 style={{ textAlign: "center" }}>
-        SignUp
+        Signup
       </Text>
-      <Input label="Email" />
-      <Input label="Password" />
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate("Login")}
+      <Input
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
       />
+
+      <Input
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Password"
+        secureTextEntry
+      />
+
+      <Button title="Signup" />
+      <TouchableOpacity
+        style={styles.link}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text style={{ color: "blue" }}>
+          Dont have a account ? Go to Sign up
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -24,5 +47,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: "80%",
     margin: 10,
+  },
+  link: {
+    marginTop: 10,
   },
 });
